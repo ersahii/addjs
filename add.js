19703b680aa98ca9a3790ae1e5170677c9ed3ddc
mirmehraj <mehraj95he@gmail.com>
2024-05-07 11:35:45 +0530

    Added multiply function

--- add.js
+++ add.js
@@ -3,5 +3,11 @@ function add(){
 	let b = 30;
 	return a +b ;
 }
-print(add());
+console.log(add());
 /*This is feature2 */
+function multiply(){
+	let a = 30;
+	let b = 40;
+	return a * b ;
+}
+console.log(multiply())
